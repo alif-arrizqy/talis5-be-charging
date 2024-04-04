659ae1c70c53431b44b94b099e98b574fef0403f
alif-arrizqy <alif.arrizqy20@gmail.com>
2024-04-04 10:15:50 +0700

    Update server.ts to dynamically set the PORT based on environment variables

--- server.ts
+++ server.ts
@@ -3,18 +3,20 @@ import { Server, prisma } from "./src/index";
 import dotenv from "dotenv";
 
 const env = dotenv.config();
+let PORT:number = 3777;
 
 if (env.parsed?.NODE_ENV === "development") {
   console.log("Running in development mode.");
   dotenv.config({ path: ".env.development" });
+  PORT = process.env.DEVELOPMENT_PORT ? parseInt(process.env.DEVELOPMENT_PORT) : 3777;
 } else if (env.parsed?.NODE_ENV === "production") {
   console.log("Running in production mode.");
   dotenv.config({ path: ".env.production" });
+  PORT = process.env.PRODUCTION_PORT ? parseInt(process.env.PRODUCTION_PORT) : 3777;
 }
 
 const app: Application = express();
 const server: Server = new Server(app);
-const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3777;
 // check connection prisma
 prisma.$connect()
   .then(() => {
