aca6ac3b63fb5a7dd26ba5c3a15898a46aa625c7
alif-arrizqy <alif.arrizqy20@gmail.com>
2024-06-25 10:23:48 +0700

    Refactor RectiController API calls

--- src/controllers/recti.controller.ts
+++ src/controllers/recti.controller.ts
@@ -8,37 +8,37 @@ class RectiController {
     const init = req.params.init;
     if (init === "true") {
       try {
-        // await axios({
-        //   method: "POST",
-        //   url: `${process.env.RECTI_URL}/set-module-32`,
-        //   data: { group: 0, value: 14 },
-        //   timeout: 5000,
-        // })
-        //   .then((response) => {
-        //     res.json(ResponseHelper.successMessage("Power module is on"));
-        //   })
-        //   .catch((error) => {
-        //     res.json(ResponseHelper.error(error.message, 500));
-        //   });
+        await axios({
+          method: "POST",
+          url: `${process.env.RECTI_URL}/set-module-32`,
+          data: { group: 0, value: 14 },
+          timeout: 5000,
+        })
+          .then((response) => {
+            res.json(ResponseHelper.successMessage("Power module is on"));
+          })
+          .catch((error) => {
+            res.json(ResponseHelper.error(error.message, 500));
+          });
         res.json(ResponseHelper.successMessage("Power module is on"));
       } catch (error) {
         res.json(ResponseHelper.error("Failed to turn on power module", 500));
       }
     } else {
       try {
-        // await axios({
-        //   method: "POST",
-        //   url: `${process.env.RECTI_URL}/set-module-32`,
-        //   data: { group: 0, value: 0 },
-        //   timeout: 5000,
-        // })
-        //   .then((response) => {
-        //     res.json(ResponseHelper.successMessage("Power module is off"));
-        //   })
-        //   .catch((error) => {
-        //     res.json(ResponseHelper.error(error.message, 500));
-        //   });
-        res.json(ResponseHelper.successMessage("Power module is off"));
+        await axios({
+          method: "POST",
+          url: `${process.env.RECTI_URL}/set-module-32`,
+          data: { group: 0, value: 0 },
+          timeout: 5000,
+        })
+          .then((response) => {
+            res.json(ResponseHelper.successMessage("Power module is off"));
+          })
+          .catch((error) => {
+            res.json(ResponseHelper.error(error.message, 500));
+          });
+        // res.json(ResponseHelper.successMessage("Power module is off"));
       } catch (error) {
         res.json(ResponseHelper.error("Failed to turn off power module", 500));
       }
@@ -66,20 +66,20 @@ class RectiController {
       if (data) {
         // post to recti api
         // set voltage
-        // await axios({
-        //   method: "POST",
-        //   url: `${process.env.RECTI_URL}/set-voltage`,
-        //   data: { group: 0, subaddress: 0, voltage: voltage },
-        //   timeout: 5000,
-        // });
+        await axios({
+          method: "POST",
+          url: `${process.env.RECTI_URL}/set-voltage`,
+          data: { group: 0, subaddress: 0, voltage: voltage },
+          timeout: 5000,
+        });
 
         // set current
-        // await axios({
-        //   method: "POST",
-        //   url: `${process.env.RECTI_URL}/set-current`,
-        //   data: { group: 0, subaddress: 0, voltage: current * 1000 },
-        //   timeout: 5000,
-        // });
+        await axios({
+          method: "POST",
+          url: `${process.env.RECTI_URL}/set-current`,
+          data: { group: 0, subaddress: 0, voltage: current * 1000 },
+          timeout: 5000,
+        });
 
         res.json(ResponseHelper.successMessage("Success update rectifier data"));
       } else {
@@ -105,24 +105,24 @@ class RectiController {
       const data = await RectiService.createVoltage(payload);
       // post to recti api
       if (data !== null) {
-        // await axios({
-        //   method: "POST",
-        //   url: `${process.env.RECTI_URL}/set-voltage`,
-        //   data: { group: 0, subaddress: 0, voltage: data },
-        //   timeout: 5000,
-        // })
-        //   .then((response) => {
-        //     res.json(
-        //       ResponseHelper.successMessage(`Success set Rectifier Voltage`)
-        //     );
-        //   })
-        //   .catch((error) => {
-        //     res.json(ResponseHelper.error(error.message, 500));
-        //   });
+        await axios({
+          method: "POST",
+          url: `${process.env.RECTI_URL}/set-voltage`,
+          data: { group: 0, subaddress: 0, voltage: data },
+          timeout: 5000,
+        })
+          .then((response) => {
+            res.json(
+              ResponseHelper.successMessage(`Success set Rectifier Voltage`)
+            );
+          })
+          .catch((error) => {
+            res.json(ResponseHelper.error(error.message, 500));
+          });
 
-        res.json(
-          ResponseHelper.successMessage(`Success set Rectifier Voltage`)
-        );
+        // res.json(
+        //   ResponseHelper.successMessage(`Success set Rectifier Voltage`)
+        // );
       } else {
         res.json(ResponseHelper.error(`Missing Rectifier Data`, 404));
       }
@@ -137,22 +137,22 @@ class RectiController {
       const data = await RectiService.createCurrent(payload);
       // post to recti api
       if (data !== null) {
-        // await axios({
-        //   method: "POST",
-        //   url: `${process.env.RECTI_URL}/set-current`,
-        //   data: { group: 0, subaddress: 0, voltage: data * 1000},
-        //   timeout: 5000,
-        // })
-        //   .then((response) => {
-        //     res.json(
-        //       ResponseHelper.successMessage(`Success set Rectifier Current`)
-        //     );
-        //   })
-        //   .catch((error) => {
-        //     res.json(ResponseHelper.error(error.message, 500));
-        //   });
+        await axios({
+          method: "POST",
+          url: `${process.env.RECTI_URL}/set-current`,
+          data: { group: 0, subaddress: 0, voltage: Number(data) * 1000 },
+          timeout: 5000,
+        })
+          .then((response) => {
+            res.json(
+              ResponseHelper.successMessage(`Success set Rectifier Current`)
+            );
+          })
+          .catch((error) => {
+            res.json(ResponseHelper.error(error.message, 500));
+          });
 
-        res.json(ResponseHelper.successMessage(`Success set Rectifier Current`));
+        // res.json(ResponseHelper.successMessage(`Success set Rectifier Current`));
       } else {
         res.json(ResponseHelper.error(`Missing Rectifier Data`, 404));
       }
